278b37ebf4f85a256a315e55235030c009744032
F3riko <vyrydy@mail.ru>
2023-08-20 19:11:20 +0300

    New API for category filtering Utils function for category filtering

--- routes/Rest API/Products API/products.js
+++ routes/Rest API/Products API/products.js
@@ -2,11 +2,22 @@ var express = require("express");
 var router = express.Router();
 const {
   getAllProducts,
+  getProductsByCategory,
 } = require("../../../db-interactions/api-products/api-products-interactions");
 
 router.get("/", async function (req, res, next) {
   try {
     const products = await getAllProducts();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.get("/category", async function (req, res, next) {
+  try {
+    const category = req.query.category;
+    const products = await getProductsByCategory(category);
     res.json(products);
   } catch (error) {
     res.status(500).json({ error: "An error occurred" });
