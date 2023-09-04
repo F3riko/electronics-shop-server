b4c6eb1b738103c429ee70647e35808035432fa7
F3riko <vyrydy@mail.ru>
2023-09-04 22:21:35 +0300

    New cart API
    Create new API route and supplementary function to get
    total price and total weight of the cart

---
+++ db-interactions/api-cart/api-calc-cart-info.js
@@ -0,0 +1,45 @@
+const connection = require("../db-init");
+
+async function calcCartInfo(cartItems) {
+  return new Promise(async (resolve, reject) => {
+    let totalSum = 0;
+    let totalWeight = 0;
+
+    for (const cartItem of cartItems) {
+      console.log(cartItem);
+      const { item_id, item_quantity } = cartItem;
+      const getItemInfoQuery = `
+        SELECT weight, (price - discount) AS discounted_price
+        FROM items
+        WHERE id = ?;
+      `;
+
+      try {
+        const itemInfo = await executeQuery(getItemInfoQuery, [item_id]);
+        if (itemInfo.length === 1) {
+          const { weight, discounted_price } = itemInfo[0];
+          totalSum += discounted_price * item_quantity;
+          totalWeight += weight * item_quantity;
+        }
+      } catch (error) {
+        reject(error);
+      }
+    }
+
+    resolve({ totalSum, totalWeight });
+  });
+}
+
+async function executeQuery(query, params) {
+  return new Promise((resolve, reject) => {
+    connection.query(query, params, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+}
+
+module.exports = calcCartInfo;
